c33cf7f3b218bb324e00faea23a6ef2f6c346719
Aayush Gokhale <aayush.gokhale@gmail.com>
2026-04-27 20:48:05 +0530

    fix: add scanBasePackages and restore spring.application.name

--- multithreading-concurrency-project-1/src/main/java/com/beingop/multithreading_concurrency_project_1/MultithreadingConcurrencyProject1Application.java
+++ multithreading-concurrency-project-1/src/main/java/com/beingop/multithreading_concurrency_project_1/MultithreadingConcurrencyProject1Application.java
@@ -3,7 +3,7 @@ package com.beingop.multithreading_concurrency_project_1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.beingop")
 public class MultithreadingConcurrencyProject1Application {
 
 	public static void main(String[] args) {
